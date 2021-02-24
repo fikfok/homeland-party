6a41eb68533e55bc16be28a6b6b9e502521ca09b
sadovenko <dasadovenko@r.media>
2021-02-24 17:42:19 +0300

    added docker

--- homeland_party/homeland_party/settings.py
+++ homeland_party/homeland_party/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'homeland_party.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'homeland-party',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': 'localhost',
+        'PORT': '5447',
     }
 }
 

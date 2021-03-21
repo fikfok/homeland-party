b39d677fdba1798af734c8c5469ca49cd809f3bc
sadovenko <dasadovenko@r.media>
2021-03-22 01:16:13 +0300

    Профиль. Карта.

--- homeland_party/homeland_party/templates/const.py
+++ homeland_party/homeland_party/templates/const.py
@@ -9,5 +9,5 @@ SMTP_SERVER = 'smtp.mail.ru'
 
 # Yandex api key
 YANDEX_API_KEY = '125708d6-baa6-407e-b906-d8be0fca76b2'
-MAP_WIDTH_PX = 600
-MAP_HEIGHT_PX = 400
+MAP_WIDTH_PX = 350
+MAP_HEIGHT_PX = 350
